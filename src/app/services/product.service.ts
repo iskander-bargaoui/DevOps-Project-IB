d053203fc9991386222d02a14a2c307920c76719
iskander <92636106+iskander-bargaoui@users.noreply.github.com>
2023-10-31 18:22:49 +0100

    test

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://192.168.126.175:8082'; 
+  private apiUrl = 'http://localhost:8082'; 
 
   constructor(private http: HttpClient) { }
 
--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -6,7 +6,7 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class StockService {
-  private apiUrl = 'http://192.168.126.175:8082';
+  private apiUrl = 'http://localhost:8082';
 
   constructor(private http: HttpClient) {
   }
